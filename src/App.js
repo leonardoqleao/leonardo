29196745b95fa4c35273f827f8b6ddc7d1191683
leonardo q <leonardoq.leao@outlook.com>
2022-02-06 12:32:15 -0300

    feat: finalizado o menu interativo em todas os tamanhos de tela.

--- src/App.js
+++ src/App.js
@@ -20,74 +20,89 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: 'idlist',
-      link: ['https://github-readme-stats.vercel.app/api/top-langs/?username=leonardoqleao&layout=demo&theme=vue&hide_border=true',
-      'https://github-readme-stats.vercel.app/api?username=leonardoqleao&count_private=true&theme=vue&show_icons=true',''],
-      teme: ['h2', 'cabecalho ccolor', 'menu-below-700 mcolor', 'mcolor-ul-li-a', 'bcolor', ''],
+      display: ['mdcolor-ul-close','button-light-dark-close','cabecalho-close',''],
+      link: ['vue', '#fff'],
+      classN: ['h2', 'cabecalho ccolor', 'menu-below-700 mcolor', 'mcolor-ul-li-a', 'bcolor',  'App','menu'],//0-6
       lightOurDark: iconDark,
     }
-    this.setState({ teme: this.state.dark })
+    this.setState({ classN: this.state.dark })
   }
   fLightOurDark = () => {
-    if (this.state.lightOurDark == iconLight) {
+    if (this.state.lightOurDark === iconLight) {
       this.setState({ lightOurDark: iconDark })
-      this.setState({ teme: ['h2', 'cabecalho ccolor', 'menu-below-700 mcolor', 'mcolor-ul-li-a', 'bcolor', 'App'] })
-      this.setState({ link: ['https://github-readme-stats.vercel.app/api/top-langs/?username=leonardoqleao&layout=demo&theme=vue&hide_border=true',
-      'https://github-readme-stats.vercel.app/api?username=leonardoqleao&count_private=true&theme=vue&show_icons=true']})
+      this.setState({ classN: ['h2', 'cabecalho ccolor', 'menu-below-700 mcolor', 'mcolor-ul-li-a', 'bcolor', 'App','menu'] })
+      this.setState({
+        link: ['vue', '#fff']
+      })
     }
     else {
       this.setState({ lightOurDark: iconLight })
-      this.setState({ teme: ['dh2', 'cabecalho cdcolor', 'menu-below-700 mdcolor', 'mdcolor-ul-li-a', 'bdcolor', 'App'] })
-      this.setState({ link: ['https://github-readme-stats.vercel.app/api/top-langs/?username=leonardoqleao&layout=demo&theme=dark&hide_border=true',
-      'https://github-readme-stats.vercel.app/api?username=leonardoqleao&count_private=true&theme=dark&show_icons=true']})
+      this.setState({ classN: ['dh2', 'cabecalho cdcolor', 'menu-below-700 mdcolor', 'mdcolor-ul-li-a', 'bdcolor', 'App','menu'] })
+      this.setState({
+        link: ['dark', '#000']
+      })
+    }
+  }
+  fMenu = () =>{
+    if (this.state.display[0] === 'mdcolor-ul-close') {
+      this.setState({ display: ['mdcolor-ul','button-light-dark','cabecalho','']})
+    }
+    else {
+      this.setState({ display: ['mdcolor-ul-close','button-light-dark-close','cabecalho-close','']})
     }
   }
 
-
   render() {
     let { lightOurDark } = this.state;
-    let ligh = this.state.teme;
+    let { classN } = this.state;
     let { display } = this.state;
     let { link } = this.state
     return (
-      <div className={ligh[5]}>
-        <header className={ligh[1]}>
-          <div className={ligh[2]}>
-            <svg id='svg' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#ffffff'><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" /></svg>
+      <div className={classN[5]}>
+        <header className={classN[1]+' '+display[2]}>
+          <div onClick={this.fMenu} className={classN[2]}>
+            <svg id='svg' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={link[1]}><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" /></svg>
           </div>
-          <nav className='menu mcolor'>
-            <ul className={display} >
-              <li><a className={ligh[3]} href='#personal'>PERSONAL STATEMENT</a>  </li>
-              <li><a className={ligh[3]} href='#experience'>EXPERIENCE</a>  </li>
-              <li><a className={ligh[3]} href='#projects'>PROJECTS</a>  </li>
-              <li><a className={ligh[3]} href='#github'>GITHUB</a>  </li>
-              <li><a className={ligh[3]} href='#repositories'>REPOSITORIES</a></li>
-              <li><a className={ligh[3]} href='#education'>EDUCATION</a>  </li>
-              <li><a className={ligh[3]} href='#contact'>CONTACT</a>  </li>
+          <nav className={classN[6]}>
+            <ul className={display[0]} >
+              <li><a onClick={this.fMenu} className={classN[3]} href='#personal'>PERSONAL STATEMENT</a>  </li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#experience'>EXPERIENCE</a>  </li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#projects'>PROJECTS</a>  </li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#github'>GITHUB</a>  </li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#repositories'>REPOSITORIES</a></li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#education'>EDUCATION</a>  </li>
+              <li><a onClick={this.fMenu} className={classN[3]} href='#contact'>CONTACT</a>  </li>
             </ul>
           </nav>
           <div onClick={this.fLightOurDark}>
-            <img src={lightOurDark} alt="lightButton" className="button-light-dark" />
+            <img src={lightOurDark} alt="lightButton" className={display[1]} />
           </div>
         </header>
-        <div className={ligh[4]}>
+        <div className={classN[4]}>
           <div className='stats'>
-            <div> <h2 className={ligh[0]} id='personal'>Personal Statemente:</h2> <Psta /> </div>
-            <div> <h2 className={ligh[0]} id='experience'>Experience:</h2> <Exp /> </div>
-            <div> <h2 className={ligh[0]} id='projects'>Projects:</h2> <Project /> </div>
-            <div>
+            
+            <div><br id='personal'/><h2 className={classN[0]}>Personal Statemente:</h2> <Psta /> </div>
+            
+            <div><br id='experience'/> <h2 className={classN[0]}>Experience:</h2> <Exp /> </div>
+            
+            <div><br id='projects'/> <h2 className={classN[0]}>Projects:</h2> <Project /> </div>
+            
+            <div><br id='github'/>
               <div>
-                <h2 className={ligh[0]} id='github'>GitHub:</h2>
+                <h2 className={classN[0]} >GitHub:</h2>
                 <div className='imgs'>
-                  <div className='most'> <img height="300em" src={link[0]} /></div>
-                  <img className='stats' height="180em" src={link[1]} />
+                  <div className='most'> <img height="300em" src={'https://github-readme-stats.vercel.app/api/top-langs/?username=leonardoqleao&layout=demo&theme='+link[0]+'&hide_border=true'} /></div>
+                  <img className='stats' height="180em" src={'https://github-readme-stats.vercel.app/api?username=leonardoqleao&count_private=true&theme='+link[0]+'&show_icons=true'} />
                 </div>
               </div>
               <Github />
             </div>
-            <div> <h2 className={ligh[0]} id='repositories'>Repositories:</h2> <Repo /> </div>
-            <div> <h2 className={ligh[0]} id='education'>Education:</h2> <Edu /> </div>
-            <div> <h2 className={ligh[0]} id='contact'>Contact:</h2> <Contact /> </div>
+            
+            <div><br id='repositories'/> <h2 className={classN[0]} >Repositories:</h2> <Repo /> </div>
+            
+            <div><br id='education'/> <h2 className={classN[0]} >Education:</h2> <Edu /> </div>
+            
+            <div><br id='contact'/> <h2 className={classN[0]} >Contact:</h2> <Contact /> </div>
           </div>
         </div>
       </div>
