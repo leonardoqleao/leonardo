4a34f67480f4f2922d9490926c9aa630a90bd7af
leonardo q <leonardoq.leao@outlook.com>
2022-02-09 10:51:48 -0300

    fix: App.js removed

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const connection = mysql.createConnection({
     password: 'LUaAul.3',
     database: 'u578105905_leonardo2',
 })
-// connection.connect(function(err){
-//     if (err){
-//         console.error('error connecting: '+ err.stack);
-//         return
-//     }
-//     console.log('connected as id ' + connection.threadId);
-// });
-// connection.query('SELECT * FROM formlql', function(err, rows, fields){
-//     if(!err){
-//         console.log('resultado: ', rows);
-//     }else{
-//         console.log('Erro ao realizar consulta');
-//     }
-// })
+connection.connect(function(err){
+    if (err){
+        console.error('error connecting: '+ err.stack);
+        return
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+connection.query('SELECT * FROM formlql', function(err, rows, fields){
+    if(!err){
+        console.log('resultado: ', rows);
+    }else{
+        console.log('Erro ao realizar consulta');
+    }
+})
